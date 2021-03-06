6542169851a9a37439bfe070450745a9cd8df30a
Poyao Wang <michael90110@gmail.com>
2021-03-06 20:08:09 +0900

    Select and delete image for workout array
    requestPermission from new module
    
    selectImage and setMainData
    
    deleteImage and setMainData

--- app/screen/WorkoutListDetailScreen.js
+++ app/screen/WorkoutListDetailScreen.js
@@ -1,5 +1,3 @@
-import React from "react";
-import NumberPicker from "../components/NumberPicker";
 import {
   Animated,
   View,
@@ -8,19 +6,51 @@ import {
   Button,
   FlatList,
   Dimensions,
+  Image,
 } from "react-native";
+import React, { useEffect, useState } from "react";
+import * as ImagePicker from "expo-image-picker";
 
 function WorkoutListDetailScreen({ route, navigation, mainData, setMainData }) {
   const { width, height } = Dimensions.get("window");
 
+  const [imageUri, setImageUri] = useState();
+
   const itemSize = Math.round(width > height ? height * 0.1 : width * 0.1);
   const selectorSize = itemSize * 5;
 
-  const finalValue = { minutes: 0, seconds: 0, numbers: 0 };
-
   const item = route.params;
 
-  const makeWorkoutsArray = () => {};
+  const requestPermission = async () => {
+    const { granted } = await ImagePicker.requestCameraPermissionsAsync();
+    if (!granted) alert("You need to enable permission to access the library.");
+  };
+
+  useEffect(() => {
+    requestPermission();
+    setImageUri(item.image == "" ? null : item.image);
+  }, []);
+
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync();
+      if (!result.cancelled) {
+        setImageUri(result.uri);
+        mainData.workoutSetup.flatListArray[item.id].image = result.uri;
+        setMainData(mainData);
+      }
+    } catch (error) {
+      console.log("Error reading an image", error);
+    }
+  };
+
+  const deleteImage = () => {
+    setImageUri(null);
+    mainData.workoutSetup.flatListArray[item.id].image = "";
+    setMainData(mainData);
+  };
+
+  // console.log(item);
 
   return (
     <View style={styles.container}>
@@ -29,9 +59,15 @@ function WorkoutListDetailScreen({ route, navigation, mainData, setMainData }) {
         style={{
           width: selectorSize,
           height: selectorSize,
-          flexDirection: "row",
         }}
-      ></View>
+      >
+        {imageUri && (
+          <Image
+            source={{ uri: imageUri }}
+            style={{ width: 200, height: 200 }}
+          />
+        )}
+      </View>
       <Button
         fontSize={30}
         title="Ok"
@@ -39,13 +75,8 @@ function WorkoutListDetailScreen({ route, navigation, mainData, setMainData }) {
           navigation.navigate("WorkoutListScreen");
         }}
       />
-      <Button
-        fontSize={30}
-        title="ValueCheck"
-        onPress={() => {
-          console.log("Value check");
-        }}
-      />
+      <Button fontSize={30} title="selectImage" onPress={selectImage} />
+      <Button fontSize={30} title="deleteImage" onPress={deleteImage} />
     </View>
   );
 }
