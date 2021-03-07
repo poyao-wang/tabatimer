0315b5b5ccb0b73edae6584915bd9b52a0edffc8
Poyao Wang <michael90110@gmail.com>
2021-03-07 13:50:51 +0900

    Refactor AppNavigator

--- App.js
+++ App.js
@@ -1,17 +1,11 @@
 import React, { useRef, useState, useEffect } from "react";
 import { Text, TouchableOpacity, Animated, Dimensions } from "react-native";
 
-import EditorScreen from "./app/screen/EditorScreen";
-import TimerScreen from "./app/screen/TimerScreen";
 import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import NavTabBar from "./app/components/NavTabBar";
-import WorkoutListScreen from "./app/screen/WorkoutListScreen";
-import EditorNavigator from "./app/navigation/EditorNavigator";
-import timerSetupDefaultData from "./app/config/timerSetupDefaultData";
-import WorkoutListNavigator from "./app/navigation/WorkoutListNavigator";
 
-const Tab = createBottomTabNavigator();
+import timerSetupDefaultData from "./app/config/timerSetupDefaultData";
+
+import AppNavigator from "./app/navigation/AppNavigator";
 
 export default function App() {
   const [tabBarShow, setTabBarShow] = useState(true);
@@ -19,34 +13,10 @@ export default function App() {
 
   return (
     <NavigationContainer>
-      <Tab.Navigator
-        tabBar={(props) => (
-          <NavTabBar {...props} tabBarShowState={tabBarShow} />
-        )}
-      >
-        <Tab.Screen name="Timer">
-          {() => (
-            <TimerScreen
-              setTabBarShow={setTabBarShow}
-              useTimerSetupState={{ timerSetup, setTimerSetup }}
-            />
-          )}
-        </Tab.Screen>
-        <Tab.Screen name="Editor">
-          {() => (
-            <EditorNavigator
-              useTimerSetupState={{ timerSetup, setTimerSetup }}
-            />
-          )}
-        </Tab.Screen>
-        <Tab.Screen name="WorkoutList">
-          {() => (
-            <WorkoutListNavigator
-              useTimerSetupState={{ timerSetup, setTimerSetup }}
-            />
-          )}
-        </Tab.Screen>
-      </Tab.Navigator>
+      <AppNavigator
+        useTabBarShowState={{ tabBarShow, setTabBarShow }}
+        useTimerSetupState={{ timerSetup, setTimerSetup }}
+      />
     </NavigationContainer>
   );
 }
---
+++ app/navigation/AppNavigator.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import { View, StyleSheet } from "react-native";
+
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import EditorDetailScreen from "../screen/EditorDetailScreen";
+import timerSetupDefaultData from "../config/timerSetupDefaultData";
+import NavTabBar from "../components/NavTabBar";
+
+import EditorScreen from "../screen/EditorScreen";
+import WorkoutListScreen from "../screen/WorkoutListScreen";
+import EditorNavigator from "../navigation/EditorNavigator";
+import WorkoutListNavigator from "../navigation/WorkoutListNavigator";
+import TimerScreen from "../screen/TimerScreen";
+
+const Tab = createBottomTabNavigator();
+
+function AppNavigator({ useTimerSetupState, useTabBarShowState }) {
+  return (
+    <Tab.Navigator
+      tabBar={(props) => (
+        <NavTabBar {...props} tabBarShowState={useTabBarShowState.tabBarShow} />
+      )}
+    >
+      <Tab.Screen name="Timer">
+        {() => (
+          <TimerScreen
+            setTabBarShow={useTabBarShowState.setTabBarShow}
+            useTimerSetupState={useTimerSetupState}
+          />
+        )}
+      </Tab.Screen>
+      <Tab.Screen name="Editor">
+        {() => <EditorNavigator useTimerSetupState={useTimerSetupState} />}
+      </Tab.Screen>
+      <Tab.Screen name="WorkoutList">
+        {() => <WorkoutListNavigator useTimerSetupState={useTimerSetupState} />}
+      </Tab.Screen>
+    </Tab.Navigator>
+  );
+}
+
+export default AppNavigator;
