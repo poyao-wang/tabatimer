54e86e56005138c850395d6bbaa71a5dcf5846ae
poyaowang <michael90110@gmail.com>
2021-09-20 19:33:36 +0900

    types for env

---
+++ types/env.d.ts
@@ -0,0 +1,17 @@
+declare module "@env" {
+  export const API_KEY: string;
+  export const AUTH_DOMAIN: string;
+  export const DATABASE_URL: string;
+  export const PROJECT_ID: string;
+  export const STORAGE_BUCKET: string;
+  export const MESSAGE_SENDER_ID: string;
+  export const APP_ID: string;
+  export const MEASUREMENT_ID: string;
+  export const FACEBOOK_APP_ID: string;
+  export const ANDROID_CLIENT_ID: string;
+  export const ANDROID_STANDALONE_APP_CLIENT_ID: string;
+  export const EXPO_CLIENT_ID: string;
+  export const IOS_CLIENT_ID: string;
+  export const IOS_STANDALONE_APP_CLIENT_ID: string;
+  export const WEB_CLIENT_ID: string;
+}
